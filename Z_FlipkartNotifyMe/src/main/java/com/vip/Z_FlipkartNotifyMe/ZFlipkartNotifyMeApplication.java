41e22d52ce9592fd8961647dd256b464aa4f38d8
Vipul Tyagi <vipultyagiabes@gmail.com>
2024-01-27 18:07:38 +0530

    FlipkartNotifyMe added via upload

---
+++ Z_FlipkartNotifyMe/src/main/java/com/vip/Z_FlipkartNotifyMe/ZFlipkartNotifyMeApplication.java
@@ -0,0 +1,13 @@
+package com.vip.Z_FlipkartNotifyMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZFlipkartNotifyMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZFlipkartNotifyMeApplication.class, args);
+	}
+
+}
---
+++ Z_FlipkartNotifyMe/src/test/java/com/vip/Z_FlipkartNotifyMe/ZFlipkartNotifyMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vip.Z_FlipkartNotifyMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZFlipkartNotifyMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
